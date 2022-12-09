36e5db7d43506650a34137f0077cafd7e12b37cd
Jorgediamanto <99408709+Jorgediamanto@users.noreply.github.com>
2022-12-09 19:30:07 +0100

    sdf

--- Ej2.py
+++ Ej2.py
@@ -7,7 +7,7 @@ def expand(expression):
     # Iterar sobre las potencias de x en el resultado expandido
     for i in range(n, -1, -1):
         # Calcular el coeficiente para el término actual
-        coefficient = n * (n - 1) * ... * (n - i + 1) / i!
+        coefficient = n * (n - 1) * ... * (n - i + 1) / math.factorial(i)
         # Añadir el término al resultado
         result += format_term(coefficient, a, i, b)
     return result
