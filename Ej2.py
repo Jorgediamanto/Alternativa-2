89f26712bc5ff4ba5fdb074f5a6fc7e55db1255a
Jorge <jorgediamanto01@gmail.com>
2022-12-09 20:36:36 +0100

    asd

--- Ej2.py
+++ Ej2.py
@@ -8,7 +8,7 @@ def expand(expression):
     # Iterar sobre las potencias de x en el resultado expandido
     for i in range(n, -1, -1):
         # Calcular el coeficiente para el término actual
-        #for f in range(m)
+        for f in range(m)
         coefficient = n * (n - 1) * ... * (n - i + 1) / math.factorial(i)
         # Añadir el término al resultado
         result += wiki.format_term(coefficient, a, i, b)
