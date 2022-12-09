7ab0cdf4b28ab6c340437ed600353d50c2acbcb8
Jorge <jorgediamanto01@gmail.com>
2022-12-09 20:35:27 +0100

    asd

--- Ej2.py
+++ Ej2.py
@@ -1,3 +1,4 @@
+import math
 
 def expand(expression):
     # Extraer a, b y n de la expresión
@@ -7,13 +8,13 @@ def expand(expression):
     # Iterar sobre las potencias de x en el resultado expandido
     for i in range(n, -1, -1):
         # Calcular el coeficiente para el término actual
+        #for f in range(m)
         coefficient = n * (n - 1) * ... * (n - i + 1) / math.factorial(i)
         # Añadir el término al resultado
-        result += format_term(coefficient, a, i, b)
+        result += wiki.format_term(coefficient, a, i, b)
     return result
 
 
-
-
+expand("(x+1)^2")   
 
 
